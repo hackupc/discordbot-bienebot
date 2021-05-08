502757eff8eaa6b8a08116d9a12e1ce987675a95
Raquel <raquel.panapalen@gmail.com>
2021-05-08 22:04:23 +0200

    Help command updated

--- modules/commands/help.py
+++ modules/commands/help.py
@@ -1,5 +1,5 @@
 from discord import Embed
-
+from discord.utils import get
 from modules.commands.base import BaseCommand
 
 
@@ -7,15 +7,24 @@ class Help(BaseCommand):
 
     async def apply(self):
         description = """
-        uwu parrot -> Random parrot
-        uwu biene -> Random biene
-        uwu biene [name] -> Biene from [name]
-        uwu cat -> Random cat
-        uwu dog -> Random dog
-        uwu joke -> bad joke (pls don't kill me :pleading_face:)
-        uwu ping -> Displays info for your internet connection :)
-        uwu meme [meme_code] text|seperated|by|lines -> inserts text to meme with code [meme_code]
+        biene parrot -> Random parrot
+        biene biene -> Random biene
+        biene biene [name] -> Biene from [name]
+        biene cat -> Random cat
+        biene dog -> Random dog
+        biene joke -> bad joke (pls don't kill me :pleading_face:)
+        biene ping -> Displays info for your internet connection :)
+        biene meme [meme_code] text|seperated|by|lines -> inserts text to meme with code [meme_code]
         (more info pinned in #random)
+        biene changeteamname [new teamname] -> Changes the teamname
+        biene jointeam [teamname] [mention all the users you want to add to the team]
         """
-        embed = Embed(title="UwU commands", description=description, color=0x00fbff)
+
+        if get(self.author.roles, name='Organizer') is not None:
+            description += """biene createteam [teamname] [mention 1 member of new team]
+            biene userinfo [mention the user you want to stalk :smirk:]  
+            biene reactions -> display a list of reactions by username and emoji from a message
+            biene addsticker [sticker name] [mention the username that receives the sticker]
+            """
+        embed = Embed(title="Biene commands", description=description, color=0x00fbff)
         await self.user.send(embed=embed)
