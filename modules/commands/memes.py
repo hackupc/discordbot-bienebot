3893b13c7eacaee40aca7b7780067e238e67c2de
EncryptEx <limusina10@gmail.com>
2021-10-07 18:09:19 +0200

    New meme help

--- modules/commands/memes.py
+++ modules/commands/memes.py
@@ -4,7 +4,7 @@ from discord import Embed
 
 from modules.commands.base import BaseCommand
 from modules.database.memes import MemesList
-from get_enviroment import COMMAND_PREFIX
+from get_enviroment import COMMAND_PREFIX, WANTS_ORIGINAL_MEME_HELP
 
 
 class Memes(BaseCommand):
@@ -67,11 +67,45 @@ class Memes(BaseCommand):
         embed = Embed()
         meme = MemesList().get_info(self.name)
         embed.title = meme['name']
-        format = ""
-        for i in range(0, int(meme['lines'])):
-            format += "text%d|" % i
-        format = format[:-1]
-        url_format = format.replace('|', '/')
-        embed.description = '**Use**: ' + COMMAND_PREFIX + ' meme %s %s' % (meme['key'], format)
-        embed.set_image(url='%s/%s.png?width=500' % (meme['url'], url_format))
+        if(str(WANTS_ORIGINAL_MEME_HELP).lower() == "true"):
+            # use the json example provided by db (not perfect because it doesn't use all the text gaps)
+            image = meme['example']
+            # once create the image, parse the text to display a proper example of how to use the command properly
+
+            # remove the .png extension 4 = len(".png")
+            nonParsedMemeText = meme['example'][:-4]
+
+            # remove the http link, we only want the TEXT.
+            # len("https://api.memegen.link/images/") = 32
+            nonParsedMemeText = nonParsedMemeText[32:]
+
+            memeexample = ""
+            # now we have to replace the first "/" to a " " meme
+            # example : rollsafe/can't_get_fired/if_you_don't_have_a_job
+            # desired: rollsafe can't_get_fired/if_you_don't_have_a_job
+            # because that's the command syntax
+            memeexample += nonParsedMemeText[:len(str(meme['key'])) + 1].replace("/", " ")
+
+            # rewrite nonParsedMemeText whithout meme name
+            nonParsedMemeText = nonParsedMemeText[len(str(meme['key'])) + 1:]
+
+            # split phrases/gaps/lines
+            for phrase in nonParsedMemeText.split("/"):
+                if (phrase != "_"):
+                    phrase = phrase.replace("_", " ")
+                memeexample += phrase + "|"
+            memeexample = memeexample[:-1]
+
+        else:
+            # use a pre-set system of filling all gaps (for loop)
+            format = ""
+            for i in range(0, int(meme['lines'])):
+                format += "text%d/" % i
+            # remove the / at the end
+            format = format[:-1]
+            # more accurate but less fun.
+            image = '%s/%s.png?width=500' % (meme['url'], format)
+            memeexample = '%s %s' % (meme['key'], format)
+        embed.description = '**Use**: ' + COMMAND_PREFIX + ' meme ' + memeexample
+        embed.set_image(url=image)
         await self.channel.send(embed=embed)
